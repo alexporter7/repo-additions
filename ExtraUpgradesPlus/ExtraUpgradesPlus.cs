8c37f1afa0d0a9ed753c2251579989762a0d2779
Alex <alex.porter7@outlook.com>
2025-05-05 10:22:04 -0500

    Initial Commit

---
+++ ExtraUpgradesPlus/ExtraUpgradesPlus.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ExtraUpgradesPlus;
+
+[BepInPlugin("alexp777.ExtraUpgradesPlus", "ExtraUpgradesPlus", "1.0")]
+public class ExtraUpgradesPlus : BaseUnityPlugin
+{
+    internal static ExtraUpgradesPlus Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Patch();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+
+    private void Update()
+    {
+        // Code that runs every frame goes here
+    }
+}
